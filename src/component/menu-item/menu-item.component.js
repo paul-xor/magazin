da2eeacf1c0cc7aad997b1461d947f434889b82c
Pasha Smolov <palexsmol@gmail.com>
2019-08-30 20:14:01 -0700

    homepage and directory components

---
+++ src/component/menu-item/menu-item.component.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./menu-item.component.js";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
